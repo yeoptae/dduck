b7535e943b82efda38784fd92056bbd9065fdb80
ooeoaudoo <ooeoaudoo@naver.com>
2019-10-30 13:02:48 +0900

    10/30

--- dduck/src/main/java/com/kh/dduck/client/controller/ClientController.java
+++ dduck/src/main/java/com/kh/dduck/client/controller/ClientController.java
@@ -1,96 +1,175 @@
 package com.kh.dduck.client.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.kh.dduck.client.model.service.ClientService;
 import com.kh.dduck.client.model.vo.Client;
 
+@SessionAttributes(value = { "loginClient" })
 @Controller
 public class ClientController {
-	
-	private Logger logger=LoggerFactory.getLogger(ClientController.class);
-	
+
+	private Logger logger = LoggerFactory.getLogger(ClientController.class);
+
 	@Autowired
 	ClientService service;
-	
-//	@Autowired
-//	BCryptPasswordEncoder pwEncoder;
-	
-	
-	
-	//로그인 뷰로 갑니다.
+
+	@Autowired
+	BCryptPasswordEncoder pwEncoder;
+
+	// 로그인 뷰로 갑니다.
 	@RequestMapping("/login/loginView.do")
 	public String login() {
 		return "login/login";
 	}
-	
-	
+
+	// 로그인시 널값이 아닐경우 이 맵핑값으로 넘어와 해당 로직 실행
+//	@RequestMapping("/client/clientLogin.do")
+//	public String login(Client c, Model model, HttpSession session) {
+//		
+//		Client result = service.selectClientOne(c);
+//		System.out.println(result+"이건 잘 되냐");
+//		String msg = "";
+//		String loc = "/";
+//
+//		if (c.getCId().equals("admin") && pwEncoder.matches(c.getCPw(), result.getCPw())) {
+//
+//			msg = "로그인 성공";
+//			session.setAttribute("loginClient", result);
+//			model.addAttribute("loginClient", result);
+//		} else if (pwEncoder.matches(c.getCPw(), result.getCPw())) {
+//			msg = "로그인 성공";
+//			session.setAttribute("loginClient", result);
+//			model.addAttribute("loginClient", result);
+//		} else {
+//
+//			msg = "로그인 실패";
+//		}
+//
+//		model.addAttribute("msg", msg);
+//		model.addAttribute("loc", loc);
+//
+//		return "common/msg";
+//	}
 	@RequestMapping("/client/clientLogin.do")
-	public String login(Client c, Model model, HttpSession session){
+	public String login(Client c, Model model, SessionStatus status) {
 
-		
+		Client result = service.selectClientOne(c);
+		System.out.println(result + "이건 잘 되냐");
+		String msg = "";
+		String loc = "/";
 
-		Client result=service.selectClientOne(c);
-		String msg="";
-		String loc="/";
-		
-		if(c.getCId().equals("admin")) {
-			
-			msg="로그인 성공";
-			session.setAttribute("loginClient", result);
-			model.addAttribute("loginClient",result);
-		}else if(c.getCId().equals("test1")) {
-			msg="로그인 성공";
-			session.setAttribute("loginClient", result);
-			model.addAttribute("loginClient",result);
-		}
-		else {
-			
-			msg="로그인 실패";
+		if (c.getCId().equals("admin") && pwEncoder.matches(c.getCPw(), result.getCPw())) {
+
+			msg = "로그인 성공";
+			model.addAttribute("loginClient", result);
+		} else if (pwEncoder.matches(c.getCPw(), result.getCPw())) {
+			msg = "로그인 성공";
+			model.addAttribute("loginClient", result);
+		} else {
+
+			msg = "로그인 실패";
 		}
 
-		model.addAttribute("msg",msg);
-		model.addAttribute("loc",loc);
-		
+		model.addAttribute("msg", msg);
+		model.addAttribute("loc", loc);
+
 		return "common/msg";
 	}
-		
 
-	
-	
-	//로그아웃
-   @RequestMapping("/Client/ClientLogout.do")
-   public String logout(HttpSession session, SessionStatus status) {
-	   
-      
-      if(!status.isComplete()) {
-         status.setComplete();
-         session.invalidate();
-         
-      }
-      logger.debug("dggddggd");
-      return "login/login";
+	// 로그아웃
+	@RequestMapping("/Client/ClientLogout.do")
+	public String logout(HttpSession session, SessionStatus status) {
 
-   }
-   //회원가입페이지
-   @RequestMapping("/Client/signUp.do")
-   public String signUp() {
-	   return "login/signup";
-   }
-   
-   
-  //회원가입
-   @RequestMapping("/Client/enroll.do")
-   public String enroll() {
-	   return "/"; 
-   }
+		if (!status.isComplete()) {
+			status.setComplete();
+			session.invalidate();
+
+		}
+		logger.debug("dggddggd");
+		return "login/login";
+
+	}
+
+	/* 회원가입 화면전환 */
+	@RequestMapping("/client/clientEnroll.do")
+	public String enroll() {
+		return "client/clientEnroll";
+	}
+
+	/* 회원가입 */
+	@RequestMapping("/client/clientEnrollEnd.do")
+	public String enrollEnd(Client c, Model model) {
+		c.setCPw(pwEncoder.encode(c.getCPw()));
+		int result = service.insertClient(c);
+		String msg = "";
+		String loc = "";
+		if (result > 0) {
+			msg = "회원가입완료";
+		} else {
+			msg = "회원가입오류";
+		}
+		model.addAttribute("msg", msg);
+		model.addAttribute("loc", loc);
+
+		return "common/msg";
+	}
+
+	/*정보수정*/
+	  @RequestMapping("/update/updateEnd.do") 
+	  public String updateEnd( Client c,Model model) { 
+		  System.out.println("updateEnd"+c);
+		  logger.debug("업데이트 됐냐?");
+		  
+		  int result = service.updateClient(c);
+			
+		  	String msg = "";
+			String loc = "";
+			if (result > 0) {
+				msg = "회원가입완료";
+			} else {
+				msg = "회원가입오류";
+			}
+			model.addAttribute("msg", msg);
+			model.addAttribute("loc", loc);
+
+			return "common/msg";
+		}
+	  
+	  /*
+	   * 마이페이지 뷰 전환
+	   * 
+	   * @RequestMapping("/update/update.do") public String modify() { return
+	   * "update/update";
+	   * 
+	   * }
+	   */
+	  
+	 
+
+	/* 내정보 출력 */
+
+	@RequestMapping("/update/update.do")
+	public String clientInfo(HttpServletRequest request, Client c, Model model) {
+		String cNo = request.getParameter("cNo");
+		System.out.println("!!!!!!!cNo" + cNo);
+		Client result = service.selectClient(c, cNo);
+//	      List<Members> members=service.selectMember(memNo);
+		model.addAttribute("client", result);
+
+		return "update/update";
+	}
+	
 
 }
--- dduck/src/main/webapp/WEB-INF/views/login/pwSearch.jsp
+++ dduck/src/main/webapp/WEB-INF/views/login/pwSearch.jsp
@@ -14,40 +14,41 @@
 <!------ Include the above in your HEAD tag ---------->
 
     <div class="container">    
+    <span style="color: green; font-weight: bold;">입력한 이메일로 받은 인증번호를 입력하세요. (인증번호가 맞아야 비밀번호를 변경하실 수 있습니다.)</span> <br> <br>  
+        <br> <br>
         <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
             <div class="panel panel-info" >
                     <div class="panel-heading">
-                        <div class="panel-title">Sign In</div>
-                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="${pageContext.request.contextPath}/pwSearch/pwSearch.do">비밀번호찾기</a></div>
+                        <div class="panel-title">메일을 입력하시면 인증번호가 발송됩니다.</div>
                     </div>     
 
                     <div style="padding-top:30px" class="panel-body" >
-
                         <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                        <form id="loginform" class="form-horizontal" role="form" action="${path }/client/clientLogin.do">
+                        <form id="pwSearch" class="form-horizontal" role="form" action="${path }/pwSearch/pwSearch2.do${dice},${e_mail}" method="post">
                                     
                             <div style="margin-bottom: 25px" class="input-group">
                                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input id="login-username" type="text" class="form-control" name="cId" value="" placeholder="User Id">                                        
+                                        <input id="login-username" type="email" class="form-control" name="e_mail" value="" placeholder="User Email">                                     
                                     </div>
-                                
-                            <div style="margin-bottom: 25px" class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                        <input id="login-password" type="password" class="form-control" name="cPw" placeholder="Password">
-                                    </div>
-                                    <c:if test="${msg == '실패'}">
+                            <!--  <div style="margin-bottom: 25px" class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                        <input id="login-username" type="text" class="form-control" name="cEmailNum" value="" placeholder="인증번호">                                     
+                                    </div> -->
+                                <%--     <c:if test="${msg == '실패'}">
                                     <tr>
 											<td colspan=2>
 											아이디 또는 패스워드가 틀렸습니다.
 											</td>
 									</tr>
-									</c:if>
+									</c:if> --%>
                                 <div style="margin-top:10px" class="form-group">
                                     <!-- Button -->
                            
                                     <div class="col-sm-12 controls">
-                                    <input type="submit" id="login" value="로그인" class="btn btn-primary">
-                                    <input type="button" value="회원가입" class="btn btn-primary">
+                                    
+                                      <button type="submit" name="submit" class="btn btn-primary">인증번호 전송</button>
+                                       <!-- <button onclick="btn_auth()" name="submit" class="btn btn-primary">인증하기</button> -->
+                                    <!-- <input type="submit" id="login" value="전송" class="btn btn-primary"> -->
                                      </div>                                    
                                    </div>
                         	</form>
@@ -56,27 +57,23 @@
               </div>
          </div>
          
- <script type="text/javascript">
- $(document).ready(function(e){
-   $('#login').click(function(){
-      
-         // 입력 값 체크
-         if($.trim($('#login-username').val()) == ''){
-            alert("아이디를 입력해 주세요.");
-            $('#login-username').focus();
-            return  false;
-         }else if($.trim($('#login-password').val()) == ''){
-            alert("패스워드를 입력해 주세요.");
-            $('#login-password').focus();
-            return false;
+<!--          <Script>
+function btn_auth(){
+    $.ajax({
+         type:"POST",
+         url:"${path}/pwSearch/auth.do",  //전송버튼 누르면 작동하는 컨트롤러 매핑값
+         data : {email : $("input[name=e_mail]").val()},  //input name값
+         success: function(data){
+          sessionStorage.setItem("authkey",data);
+         },error : function(data){
+            console.log(data);
          }
+     });
+     alert("인증번호를 발송했습니다.");
+ }
+   
+</Script>
+          -->
          
-       //전송
-         $('#loginform').submit(); 
-      });
- });
-      
-</script>
          
-    
 <jsp:include page="/WEB-INF/views/common/footer.jsp"/>
